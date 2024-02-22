4e487060351d3ca25de790bb764ac2304e631de1
WillMatthews <will.a.matthews@me.com>
2024-02-22 06:41:54 +0000

    remove example files

--- site.hs
+++ site.hs
@@ -15,13 +15,7 @@ main = hakyll $ do
         route   idRoute
         compile compressCssCompiler
 
-    match (fromList ["about.rst", "contact.markdown"]) $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
-
-    match "posts/*" $ do
+    match (fromList ["posts/index.md", "posts/me.md"]) $ do
         route $ setExtension "html"
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/post.html"    postCtx
@@ -29,63 +23,57 @@ main = hakyll $ do
             >>= loadAndApplyTemplate "templates/default.html" postCtx
             >>= relativizeUrls
 
-    create ["atom.xml"] $ do
-        route idRoute
-        compile $ do
-            let feedCtx = postCtx `mappend`
-                constField "description" "This is the post description"
-
-            posts <- fmap (take 10) . recentFirst =<<
-                 loadAllSnapshots "posts/*" "content"
-            renderAtom myFeedConfiguration feedCtx posts
-
-    create ["index.xml"] $ do
-        route idRoute
-        compile $ do
-            let feedCtx = postCtx `mappend`
-                constField "description" "This is the post description"
-
-            posts <- fmap (take 10) . recentFirst =<<
-                loadAllSnapshots "posts/*" "content"
-            renderRss myFeedConfiguration feedCtx posts
-
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
-
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
-
-
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    defaultContext
-
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
+    -- match "posts/*" $ do
+    --     route $ setExtension "html"
+    --     compile $ pandocCompiler
+    --         >>= loadAndApplyTemplate "templates/post.html"    postCtx
+    --         >>= saveSnapshot "content"
+    --         >>= loadAndApplyTemplate "templates/default.html" postCtx
+    --         >>= relativizeUrls
+    --
+    -- create ["atom.xml"] $ do
+    --     route idRoute
+    --     compile $ do
+    --         let feedCtx = postCtx `mappend`
+    --             constField "description" "This is the post description"
+    --
+    --         posts <- fmap (take 10) . recentFirst =<<
+    --              loadAllSnapshots "posts/*" "content"
+    --         renderAtom myFeedConfiguration feedCtx posts
+    --
+    -- create ["index.xml"] $ do
+    --     route idRoute
+    --     compile $ do
+    --         let feedCtx = postCtx `mappend`
+    --             constField "description" "This is the post description"
+    --
+    --         posts <- fmap (take 10) . recentFirst =<<
+    --             loadAllSnapshots "posts/*" "content"
+    --         renderRss myFeedConfiguration feedCtx posts
+    --
+    -- create ["archive.html"] $ do
+    --     route idRoute
+    --     compile $ do
+    --         posts <- recentFirst =<< loadAll "posts/*"
+    --         let archiveCtx =
+    --                 listField "posts" postCtx (return posts) `mappend`
+    --                 constField "title" "Archives"            `mappend`
+    --                 defaultContext
+    --
+    --         makeItem ""
+    --             >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+    --             >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+    --             >>= relativizeUrls
 
     match "templates/*" $ compile templateBodyCompiler
 
 
 --------------------------------------------------------------------------------
+
 postCtx :: Context String
 postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
+    dateField "created" "%Y-%m-%d" `mappend`
     defaultContext
-
 
 myFeedConfiguration :: FeedConfiguration
 myFeedConfiguration = FeedConfiguration
